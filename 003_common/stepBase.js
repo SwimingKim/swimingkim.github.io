5804ab06777201f961ea89bc3829bc098f0a4043
skim <rumex13@naver.com>
2018-01-10 10:34:10 +0900

    Add files via upload

---
+++ 003_common/stepBase.js
@@ -0,0 +1,19 @@
+/**
+created by NOD
+pageBase
+천재교육 진도 학습 콘텐츠 GNB 메뉴 및 플레이어 연동 관리
+*/
+
+( function ( sb ) {
+
+    sb.init = function() {
+        Util.log( "이러면 아니되오" );
+    }
+
+    sb.test = function() {
+        Util.log( "WOW" );
+    }
+
+})( stepBase = stepBase || {} );
+var stepBase;
+
