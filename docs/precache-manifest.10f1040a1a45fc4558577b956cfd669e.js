a166941ac3a4c3d90a97f06588c049c60acccc51
skim <rumex13@naver.com>
2021-05-02 21:40:35 +0900

    add portfolio

---
+++ docs/precache-manifest.10f1040a1a45fc4558577b956cfd669e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff0042a591137afc4118690cc4fd289",
+    "url": "/canvas.html"
+  },
+  {
+    "revision": "34e06ac2ca7576170677",
+    "url": "/css/app.653ad252.css"
+  },
+  {
+    "revision": "19a247eeb2bca8a3d7ea",
+    "url": "/css/chunk-vendors.6e6b8680.css"
+  },
+  {
+    "revision": "72dab2375dd8cfc255eb8abda7c6669e",
+    "url": "/img/1.72dab237.jpg"
+  },
+  {
+    "revision": "3672409e789ca6c6105e4ba453578ad5",
+    "url": "/img/10.3672409e.jpg"
+  },
+  {
+    "revision": "3854d6b8cf1c7b0795a4c17f6c147fb7",
+    "url": "/img/2.3854d6b8.jpg"
+  },
+  {
+    "revision": "d363a2f8b3ee14eff443489f446449e1",
+    "url": "/img/3.d363a2f8.jpg"
+  },
+  {
+    "revision": "03e39ce148613c24f750e3ec242b0182",
+    "url": "/img/4.03e39ce1.jpg"
+  },
+  {
+    "revision": "f5c334eaba30b3fa39ab57fbc7d796da",
+    "url": "/img/5.f5c334ea.jpg"
+  },
+  {
+    "revision": "08962120b33e30ed60712336672df53c",
+    "url": "/img/6.08962120.jpg"
+  },
+  {
+    "revision": "c402150026dbc0f9921129e86ccb1bb8",
+    "url": "/img/7.c4021500.jpg"
+  },
+  {
+    "revision": "38356472248792820afc5c733e72afc2",
+    "url": "/img/8.38356472.jpg"
+  },
+  {
+    "revision": "50e0c69f27f89f4b2c0a946d6752b69b",
+    "url": "/img/9.50e0c69f.jpg"
+  },
+  {
+    "revision": "f73fe9b6e371cb7e39b9aed9efe3ec78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34e06ac2ca7576170677",
+    "url": "/js/app.98e95702.js"
+  },
+  {
+    "revision": "19a247eeb2bca8a3d7ea",
+    "url": "/js/chunk-vendors.c5adf3ee.js"
+  },
+  {
+    "revision": "4e30ac7294b72008fd3a127b6ef8c2d4",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
